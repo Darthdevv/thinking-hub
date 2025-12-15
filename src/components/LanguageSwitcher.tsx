4ed283f293eb5c2f6ecf01ba583f74111d474ed4
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-12-15 13:10:38 +0200

    changes

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,70 +1,26 @@
 "use client";
 
-import { useState } from "react";
-import { ChevronDown } from "lucide-react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-// import ksa from "../assets/images/ksaa.png";
 import usa from "../assets/images/usa.png";
 
 export default function LanguageDropdown() {
   const { i18n } = useTranslation();
-  const [open, setOpen] = useState(false);
 
-  const changeLanguage = (lang: "en" | "ar") => {
-    i18n.changeLanguage(lang);
-    setOpen(false);
-  };
-
-  const languages = [
-    {
-      code: "en",
-      label: "English",
-      flag: usa,
-    },
-    // {
-    //   code: "ar",
-    //   label: "العربية",
-    //   flag: ksa,
-    // },
-  ];
-
-  const active = languages.find((l) => l.code === i18n.language);
+  useEffect(() => {
+    if (i18n.language !== "en") {
+      i18n.changeLanguage("en");
+    }
+  }, [i18n]);
 
   return (
-    <div className="relative">
-      {/* Trigger button */}
-      <button
-        onClick={() => setOpen(!open)}
-        className="flex items-center gap-2 text-base font-normal  hover:border-b-2 hover:border-[#57B3B9] transition"
-      >
-        <img
-          src={active?.flag}
-          className="w-4.5 h-4.5 rounded-full object-cover"
-          alt=""
-        />
-        <span>{active?.code === "en" ? "Eng" : "Ar"}</span>
-        <ChevronDown className="w-4 h-4" />
-      </button>
-
-      {/* Dropdown */}
-      {open && (
-        <div className="absolute right-0 mt-2 w-36 bg-[#000201CC] border border-[#57B3B9] rounded-lg shadow-lg py-2 z-50">
-          {languages.map((lang) => (
-            <button
-              key={lang.code}
-              onClick={() => changeLanguage(lang.code as "en" | "ar")}
-              className="w-full flex items-center gap-3 px-3 py-2 hover:bg-zinc-800 text-left"
-            >
-              <img
-                src={lang.flag}
-                className="w-4.5 h-4.5 rounded-full object-cover"
-                alt=""
-              />
-              <span>{lang.label}</span>
-            </button>
-          ))}
-        </div>
-      )}
+    <div className="flex items-center gap-2 text-base font-normal">
+      <img
+        src={usa}
+        className="w-4.5 h-4.5 rounded-full object-cover"
+        alt="English"
+      />
+      <span>Eng</span>
     </div>
   );
 }
